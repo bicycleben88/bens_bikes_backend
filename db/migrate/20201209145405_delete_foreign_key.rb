434b166a6914b324e3714d76e7db9b09fddb71c3
Ben Higginbotham <bicycleben88@gmail.com>
2021-03-19 16:15:31 -0400

    scaffold orderItem

--- db/migrate/20201209145405_delete_foreign_key.rb
+++
@@ -1,6 +0,0 @@
-class DeleteForeignKey < ActiveRecord::Migration[6.0]
-  def change
-    remove_foreign_key :items, :orders
-    remove_column :items, :order_id
-  end
-end
--- db/migrate/20201211022830_edit_orders_column.rb
+++
@@ -1,6 +0,0 @@
-class EditOrdersColumn < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :orders, :qty
-    add_column :orders, :total, :decimal, precision: 10, scale: 2
-  end
-end
