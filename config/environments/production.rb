af335f54c2ec9468e670d2cc4343f5b043b11aa8
Ben Higginbotham <bicycleben88@gmail.com>
2021-04-14 20:45:50 -0400

    change user_name to api key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,8 +111,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => 'apikey',
+    :password       => ENV['SEND_GRID_API_KEY'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
