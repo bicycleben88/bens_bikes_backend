d23c64b5468f988ad7a1b3fce1069cd3ca340df7
Ben Higginbotham <bicycleben88@gmail.com>
2026-01-03 21:34:35 -0500

    Configure CORS to allow Vercel and local origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,14 +3,14 @@
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
 
-# Read more: https://github.com/cyu/rack-cors
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # Allow requests from your live Vercel app and local development environments
+    origins 'https://bens-bikes-frontend.vercel.app', 'localhost:3000', 'localhost:4000'
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true # This is required if your frontend uses cookies or authorization headers
+  end
+end
